6335bec065aa45670b8eb86306209281395d0efa
Marlon Marcos <marlon.marcos21@gmail.com>
2016-08-07 00:37:59 +0800

    Fix permissions for user model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,20 +1,38 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
-    user ||= User.new
+  attr_reader :current_user
 
-    if user.persisted? && user.id == 1
+  def initialize(user)
+    @current_user = user || User.new
+
+    if current_user.persisted? && current_user.id == 1
       can :manage, :all
       return
     end
 
+    post_permissions
+    user_permissions
+  end
+
+  private
+
+  def post_permissions
     can :read, Post
     can [:update, :destroy, :publish, :unpublish], Post do |post|
-      post.user == user
+      post.user == current_user
     end
     can [:create, :upload_image, :remove_image], Post do
-      user.persisted?
+      current_user.persisted?
+    end
+  end
+
+  def user_permissions
+    can :read, User do |user|
+      current_user.persisted?
+    end
+    can :update, User do |user|
+      user == current_user
     end
   end
 end
