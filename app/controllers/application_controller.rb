647f98cb507b74999541aa7be75e1a3b0de4f59e
Marlon Marcos <marlon.marcos21@gmail.com>
2016-08-07 01:13:13 +0800

    Make nabber menu urls dynamic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :in_homepage?
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
+  end
+
+  def in_homepage?
+    request.original_fullpath == '/'
   end
 
   protected
