b2a949f94a7d3c07a9bb0bedf17fc7373758bf76
Marlon Marcos <marlon.marcos21@gmail.com>
2016-08-09 15:05:50 +0800

    Added animation when clicking on nav bar menus

--- app/assets/javascripts/my_theme.js
+++ app/assets/javascripts/my_theme.js
@@ -6,10 +6,10 @@
 
 // jQuery to collapse the navbar on scroll
 function collapseNavbar() {
-  if ($(".navbar").offset().top > 50) {
-    $(".navbar-fixed-top").addClass("top-nav-collapse");
+  if ($('.navbar').offset().top > 50) {
+    $('.navbar-fixed-top').addClass('top-nav-collapse');
   } else {
-    $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    $('.navbar-fixed-top').removeClass('top-nav-collapse');
   }
 }
 
@@ -19,6 +19,14 @@ $(document).ready(collapseNavbar);
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
   $('a.navbar-brand').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
+
+  $('a.page-scroll').bind('click', function(event) {
     var $anchor = $(this);
     $('html, body').stop().animate({
       scrollTop: $($anchor.attr('href')).offset().top
