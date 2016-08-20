249f890060da61b4c300cec84ae92c6e82208bd9
Marlon Marcos <marlon.marcos21@gmail.com>
2016-08-20 17:20:06 +0800

    Fix user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,7 +70,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
     if @user.save
       redirect_to @user, notice: 'User was successfully created!'
     else
