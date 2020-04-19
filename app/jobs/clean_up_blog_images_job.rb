1cc7b1340a432cffce364be3a357101b4a5d2f7f
Marlon Marcos <marlon.marcos21@gmail.com>
2020-04-19 03:34:38 -0400

    syntax

--- app/jobs/clean_up_blog_images_job.rb
+++ app/jobs/clean_up_blog_images_job.rb
@@ -3,9 +3,10 @@ class CleanUpBlogImagesJob < ApplicationJob
 
   def perform(blog_id)
     blog = Blog.find blog_id
-    file_names    = get_blog_images(blog)
-    unused_images = BlogMedium.where(attachable: blog)
-                              .where.not(source_file_name: file_names)
+    file_names = get_blog_images(blog)
+    unused_images = BlogMedium
+                      .where(attachable: blog)
+                      .where.not(source_file_name: file_names)
 
     unused_images.delete_all if unused_images.exists?
   end
@@ -13,7 +14,7 @@ class CleanUpBlogImagesJob < ApplicationJob
   private
 
   def get_blog_images(blog)
-    html    = Nokogiri::HTML(blog.body)
+    html = Nokogiri::HTML(blog.body)
     sources = html.xpath '//img/@src'
     sources.map do |attr|
       src = attr.value
