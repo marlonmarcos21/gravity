8b0339a5cc4a39561ee08d3733465c56bd4cf4d0
= <marlon.marcos21@gmail.com>
2020-08-10 23:08:25 -0400

    Update data seeder

--- lib/data_seeder.rb
+++ lib/data_seeder.rb
@@ -1,5 +1,26 @@
 module DataSeeder
   class << self
+    def users
+      password = '123123123'
+      user1 = User.create(
+        email: 'user1@example.com',
+        first_name: 'First',
+        last_name: 'User',
+        password: password,
+        password_confirmation: password
+      )
+      UserProfile.create(user: user1)
+
+      user2 = User.create(
+        email: 'user2@example.com',
+        first_name: 'Second',
+        last_name: 'User',
+        password: password,
+        password_confirmation: password
+      )
+      UserProfile.create(user: user2)
+    end
+
     def posts(count = 50)
       count.times do
         Post.create(
