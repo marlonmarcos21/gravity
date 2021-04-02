221a829a274e1ac446c1e0b810b8495fffe79dc6
= <marlon.marcos21@gmail.com>
2021-04-02 19:32:14 -0400

    Stupid ES6

--- app/assets/javascripts/trix_upload.js
+++ app/assets/javascripts/trix_upload.js
@@ -1,22 +1,22 @@
 function uploadAttachment(attachment) {
-  const file = attachment.file;
-  const form = new FormData;
+  var file = attachment.file;
+  var form = new FormData;
 
   form.append('Content-Type', file.type);
   form.append('recipe_medium[file]', file);
 
-  const xhr = new XMLHttpRequest;
+  var xhr = new XMLHttpRequest;
   xhr.open('POST', '/recipe_media.json', true);
   xhr.setRequestHeader('X-CSRF-Token', Rails.csrfToken());
 
   xhr.upload.onprogress = function(event) {
-    const progress = event.loaded / event.total * 100;
+    var progress = event.loaded / event.total * 100;
     attachment.setUploadProgress(progress);
   }
 
   xhr.onload = function() {
     if (xhr.status === 201) {
-      const data = JSON.parse(xhr.responseText);
+      var data = JSON.parse(xhr.responseText);
       return attachment.setAttributes({
         url: data.url,
         href: data.url,
@@ -29,7 +29,7 @@ function uploadAttachment(attachment) {
 }
 
 document.addEventListener('trix-attachment-add', function(event) {
-  const attachment = event.attachment;
+  var attachment = event.attachment;
 
   if (attachment.file) {
     return uploadAttachment(attachment);
