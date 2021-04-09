6ae3d800ead8710e2b2de7c58229be604304b5e6
= <marlon.marcos21@gmail.com>
2021-04-08 23:50:37 -0400

    Update host

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -10,11 +10,14 @@ s3_opts = {
 }
 
 Shrine.storages = {
-  cache: Shrine::Storage::S3.new(upload_options: { acl: "public-read" }, prefix: 'cache', **s3_opts),
-  store: Shrine::Storage::S3.new(upload_options: { acl: "public-read" }, prefix: 'store', **s3_opts)
+  cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_opts),
+  store: Shrine::Storage::S3.new(prefix: 'store', **s3_opts)
 }
 
 Shrine.plugin :activerecord           # loads Active Record integration
 Shrine.plugin :cached_attachment_data # enables retaining cached file across form redisplays
 Shrine.plugin :restore_cached_data    # extracts metadata for assigned cached files
 Shrine.plugin :determine_mime_type    # determine mime_type
+
+Shrine.plugin :upload_options, acl: 'public-read'
+Shrine.plugin :url_options, store: { host: "https://#{ENV['AWS_S3_BUCKET']}" }
